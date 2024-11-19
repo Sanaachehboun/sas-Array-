df24c97140a21e3244943ad7d61deeff1aac0f86
Sanaa Abn <abnsanaa480@gmail.com>
2024-11-19 15:39:17 +0100

    exercice3

--- Ex.js
+++ Ex.js
@@ -1,9 +1,15 @@
-// 
+// Exercice Facile :
 // exercice 1
 let nombre =[1,2,3,4,5];
 console.log(nombre);
-
-
 //exercice 2  
 let fruits=["apple","banana","cherry"];
 console.log(fruits[1]);
+// exercice 3 
+let Nombres=[10,20, 30,40];
+Nombres[2]=50; 
+console.log(Nombres);
+// exercice 4 
+let Fruits=["mango","peach","pear"];
+Fruits[3]="pomme";
+console.log(Fruits);
